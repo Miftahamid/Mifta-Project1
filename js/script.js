b583f44c54baa82897c37332580722cdcbd3b4a4
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-01-09 16:23:00 -0500

    5  commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
-var card = document.getElementById('show');
- console.log(card)
-document.getElementById('show').addEventListener('click',
  
-function() {
-     card.classList.toggle('flipped');
+
+let  card = document.getElementById('card');
+
+document.getElementById('flip').addEventListener('click', function() {
+    card.classList.toggle('flipped');
 }, false);
+
+function toggle_visibility(card) {
+    var e = document.getElementById(card);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
+
+ var question= 1;
+ showSlides(question);
+
+function plusSlides(n) {
+  showSlides(question += n);
+}
+
+function currentSlide(n) {
+  showSlides(question = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
